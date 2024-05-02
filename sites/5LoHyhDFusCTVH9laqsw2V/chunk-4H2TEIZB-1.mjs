ac4dd00d819aa5d97c2fb1b035584769f7b6ac46
valentin19671 <150732112+valentin19671@users.noreply.github.com>
2024-05-02 13:30:50 +0300

    1

---
+++ sites/5LoHyhDFusCTVH9laqsw2V/chunk-4H2TEIZB-1.mjs
@@ -0,0 +1,2 @@
+var e={ZBF_alj1w:{borderRadius:0,bottomLeftRadius:0,bottomRightRadius:0,isMixedBorderRadius:!1,isRed:!0,topLeftRadius:0,topRightRadius:0}},r={};export{e as a,r as b};
+//# sourceMappingURL=chunk-4H2TEIZB.mjs.map
---
+++ sites/5LoHyhDFusCTVH9laqsw2V/chunk-RXHLNPWQ-1.mjs
@@ -0,0 +1,51 @@
+var t=e=>typeof e=="string"?e:String(e),n=e=>typeof e!="string"?"":e.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/"/g,"&quot;").replace(/'/g,"&#39;"),i=e=>JSON.stringify(e);function w(e,A){let o=e?.zpGuVG3u8,d=e?.dS4xVhYiZ,u=e?.SYNDuS4VU,c=e?.DpWS024Jm,r=e?.EzfYrWBc0,l=e?.hpR3EhuDR,s=e?.MCO0r36WL,f=e?.jidZYOyFp,v=e?.NUSzn2WjA,h=e?.AjM_uJtPq,y=e?.W_HeDvNSh,S=e?.EkK2rXe34,p=e?.imL_ZS2VC;return{bodyClassName:"framer-body-jBb9zfRl6",breakpoints:[{hash:"1u7oq2f",mediaQuery:"(min-width: 1600px)"},{hash:"1drj7n8",mediaQuery:"(min-width: 1200px) and (max-width: 1599px)"},{hash:"1i2f3x8",mediaQuery:"(min-width: 720px) and (max-width: 1199px)"},{hash:"1jzze0g",mediaQuery:"(max-width: 719px)"}],customHTMLHeadStart:`<link rel="canonical" href="https://cron.design/studiya-veb-dizayna/${u!==void 0?t(n(u)):"{{SYNDuS4VU}}"}">
+
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "FAQPage",
+  "mainEntity": [
+    {
+      "@type": "Question",
+      "name": ${c!==void 0?t(i(n(c))):"{{DpWS024Jm | json}}"},
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": ${r!==void 0?t(i(n(r))):"{{EzfYrWBc0 | json}}"}
+      }
+    },
+    {
+      "@type": "Question",
+      "name": ${l!==void 0?t(i(n(l))):"{{hpR3EhuDR | json}}"},
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": ${s!==void 0?t(i(n(s))):"{{MCO0r36WL | json}}"}
+      }
+    },
+    {
+      "@type": "Question",
+      "name": ${f!==void 0?t(i(n(f))):"{{jidZYOyFp | json}}"},
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": ${v!==void 0?t(i(n(v))):"{{NUSzn2WjA | json}}"}
+      }
+    },
+    {
+      "@type": "Question",
+      "name": ${h!==void 0?t(i(n(h))):"{{AjM_uJtPq | json}}"},
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": ${y!==void 0?t(i(n(y))):"{{W_HeDvNSh | json}}"}
+      }
+    },
+    {
+      "@type": "Question",
+      "name": ${S!==void 0?t(i(n(S))):"{{EkK2rXe34 | json}}"},
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": ${p!==void 0?t(i(n(p))):"{{imL_ZS2VC | json}}"}
+      }
+    }
+  ]
+}
+<\/script>`,description:`${d!==void 0?t(d):"{{dS4xVhYiZ}}"}`,elements:{},framerSearch:{index:!0},title:`${o!==void 0?t(o):"{{zpGuVG3u8}}"}`,viewport:"width=device-width"}}var g=1,z={exports:{default:{type:"function",annotations:{framerContractVersion:"1"}},metadataVersion:{type:"variable",annotations:{framerContractVersion:"1"}},__FramerMetadata__:{type:"variable"}}};export{w as a,g as b,z as c};
+//# sourceMappingURL=chunk-RXHLNPWQ.mjs.map
